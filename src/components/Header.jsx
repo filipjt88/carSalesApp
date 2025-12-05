7f5bfa5b4db550e2ddcc55c6a1fd635ba5224bab
filipjt88 <filip88ks@gmail.com>
2025-12-05 20:53:44 +0100

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link to="/" className="navbar-brand fw-world">
+                Car market
+                </Link>
+                <form className="d-flex ms-auto" style={{maxWidth:"400px"}}>
+                    <input className="form-control me-2" type="search" placeholder="Pretraga..." />
+                    <button className="btn btn-outline-light" type="submit">Trazi</button>
+                </form>
+            </div>
+        </nav>
+    )
+}
