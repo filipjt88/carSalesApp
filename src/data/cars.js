771256357557e4c04e4fcbf1fd4f0dc8cd913d81
filipjt88 <filip88ks@gmail.com>
2025-12-05 21:00:44 +0100

    demo data cars

---
+++ src/data/cars.js
@@ -0,0 +1,31 @@
+export const cars = [
+    {
+        id: 1,
+        brend: "BMW",
+        model: "530d",
+        year: "2018",
+        km: "210000",
+        image: "https://flywheelcars.com/wp-content/uploads/2023/08/WhatsApp-Image-2023-08-14-at-4.49.18-PM-1.jpeg",
+        city: "Beograd"
+    },
+    {
+        id: 2,
+        brend: "Audi",
+        model: "A4",
+        year: "2016",
+        km: "170000",
+        image: "https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/fe905ab05f2ec68b0c5e1201903d9bd0",
+        city: "Novi Sad"
+    },
+      {
+        id: 3,
+        brend: "Mercedes-Benz",
+        model: "E class",
+        year: "2019",
+        km: "110000",
+        image: "https://media.ed.edmunds-media.com/mercedes-benz/e-class/2019/oem/2019_mercedes-benz_e-class_sedan_amg-e-63-s_fq_oem_8_1600.jpg",
+        city: "Nis"
+    },
+    
+
+]
