33a1078b3606010cfe3b06bfb482e2445f7fd1a9
filipjt88 <filip88ks@gmail.com>
2025-12-05 21:16:48 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "ract-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 
