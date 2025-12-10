a9452d1f3d4be885dd821cb366b2c384ee057924
filipjt88 <filip88ks@gmail.com>
2025-12-10 21:48:10 +0100

    added

--- src/data/cars.js
+++ src/data/cars.js
@@ -5,6 +5,7 @@ export const cars = [
         model: "530d",
         year: "2018",
         km: "210000",
+        price: "35.000",
         image: "https://flywheelcars.com/wp-content/uploads/2023/08/WhatsApp-Image-2023-08-14-at-4.49.18-PM-1.jpeg",
         city: "Beograd"
     },
@@ -14,6 +15,7 @@ export const cars = [
         model: "A4",
         year: "2016",
         km: "170000",
+        price: "25.000",
         image: "https://media.drive.com.au/obj/tx_q:70,rs:auto:1920:1080:1/caradvice/private/fe905ab05f2ec68b0c5e1201903d9bd0",
         city: "Novi Sad"
     },
@@ -23,6 +25,7 @@ export const cars = [
         model: "E class",
         year: "2019",
         km: "110000",
+        price: "45.000",
         image: "https://media.ed.edmunds-media.com/mercedes-benz/e-class/2019/oem/2019_mercedes-benz_e-class_sedan_amg-e-63-s_fq_oem_8_1600.jpg",
         city: "Nis"
     },
@@ -32,6 +35,7 @@ export const cars = [
         model: "Insignia",
         year: "2022",
         km: "97000",
+        price: "22.000",
         image: "https://nova.rs/wp-content/uploads/2022/07/22/1658485478-AKTUELNO-Opel-Insignia_GSi-2018-1600-02-1200x800.jpg",
         city: "Novi Pazar"
     },
@@ -41,6 +45,7 @@ export const cars = [
         model: "Carrera",
         year: "2023",
         km: "74000",
+        price: "95.000",
         image: "https://cdn.motor1.com/images/mgl/ZnKvO2/s1/2023-porsche-911-carrera-t-in-gulf-blue.jpg",
         city: "Beograd"
     },
@@ -50,6 +55,7 @@ export const cars = [
         model: "Q8",
         year: "2018",
         km: "250000",
+        price: "65.000",
         image: "https://upload.wikimedia.org/wikipedia/commons/f/f0/2018_Audi_Q8.jpg",
         city: "Zrenjanin"
     },
