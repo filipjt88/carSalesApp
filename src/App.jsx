f8b77d3cb8f969f1f79947d580714e96c1a6d8fb
filipjt88 <filip88ks@gmail.com>
2025-12-11 20:44:06 +0100

    app preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
+import CarDetails from "./components/pages/CarDetails";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/car/:id" element={<CarDetails/>} />
       </Routes>
     </BrowserRouter>
   );
