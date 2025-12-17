0f3c2b653996eddb7be037b115d2dccdf463583a
filipjt88 <filip88ks@gmail.com>
2025-12-17 21:54:30 +0100

    save

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,9 +4,7 @@ import CarCard from "../../components/CarCard";
 export default function Home() {
     return (
         <div className="container mt-4">
-            <h2 className="mb-4 text-center">
-                Automobili na prodaju 
-            </h2>
+            <h2 className="mb-4 text-center">Automobili na prodaju</h2>
             <div className="row">
                 {cars.map(car => (
                     <CarCard key={car.id} car={car}/>
