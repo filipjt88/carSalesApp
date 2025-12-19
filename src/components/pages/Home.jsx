40e4a7b605e27576003e168a748bd788135d96d4
filipjt88 <filip88ks@gmail.com>
2025-12-19 20:36:54 +0100

    add search filters

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,50 @@
-import { cars } from "../../data/cars";
+import { cars as allCars } from "../../data/cars";
 import CarCard from "../../components/CarCard";
+import { useState } from "react";
+import SearchBox from "../SearchBox";
 
 export default function Home() {
+        const [cars, setCars] = useState(allCars);
+        
+        const handleSearch = (filters) => {
+            let result = allCars;
+
+            if(filters.brand !== "Sve marke") {
+                result = result.filter(c => c.brand === filters.brand);
+            }
+
+            if(filters.model) {
+                result = result.filter(c => c.model === filter.model);
+            }
+
+            if(filters.fuel !== "Sva goriva") {
+                result = result.filter(c => c.fuel === filters.fuel);
+            }
+
+            if(filters.priceTo) {
+                result = result.filter(c => c.price <= Number(filters.priceTo));
+            }
+
+            if(filters.yearsFrom) {
+                result = result.filter(c => c.year >= Number(filters.yearsFrom));
+            }
+
+            if(filters.yearsTo) {
+                result = result.filter(c => c.year <= Number(filters.yearsTo));
+            }
+
+            setCars(result);
+        };
+
     return (
-        <div className="container mt-4">
+         <div className="container mt-4">
+
+            <SearchBox OnSearch={handleSearch} />
             <h2 className="mb-4 text-center">Automobili na prodaju</h2>
             <div className="row">
+                {cars.length === 0 && (
+                    <p className="text-muted">Nema rezultata za izabrane kriterijume!</p>
+                )}
                 {cars.map(car => (
                     <CarCard key={car.id} car={car}/>
                 ))}
@@ -13,3 +52,7 @@ export default function Home() {
         </div>
     );
 }
+
+
+
+ 
