8a90d82230c016381fc69a16d3878ac45fb23c63
filipjt88 <filip88ks@gmail.com>
2025-12-21 20:55:00 +0100

    create register

---
+++ backend/api/register.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../config/db.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if(
+    empty($data['name']) || empty($data['email']) || empty($data['password'])
+) {
+    http_response_code(400);
+    echo json_encode(['error' => "Sva polja su obavezna!"]);
+    exit;
+}
+
+$name     = trim($data['name']);
+$email    = trim($data['email']);
+$password = password_hash($data['password'], PASSWORD_DEFAULT);
+
+try {
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+    $stmt->execute([
+        "name" => $name,
+        "email" => $email,
+        "password" => $password
+    ]);
+
+    echo json_encode(["success" => true]);
+} catch (PDOException $e) {
+    http_response_code(409);
+    echo json_encode(["error" => "Email vec postoji!"]);
+}
