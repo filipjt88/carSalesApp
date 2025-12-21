47fc343471ebe6a5095510811c2f203b8bdc4251
filipjt88 <filip88ks@gmail.com>
2025-12-21 21:11:53 +0100

    Save

--- backend/api/register.php
+++ backend/api/register.php
@@ -7,7 +7,7 @@ if(
     empty($data['name']) || empty($data['email']) || empty($data['password'])
 ) {
     http_response_code(400);
-    echo json_encode(['error' => "Sva polja su obavezna!"]);
+    echo json_encode(["error" => "Sva polja su obavezna!"]);
     exit;
 }
 
@@ -16,7 +16,7 @@ $email    = trim($data['email']);
 $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
 try {
-    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
     $stmt->execute([
         "name" => $name,
         "email" => $email,
