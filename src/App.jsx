4dc849b331872bd0ce0791d70454d36a4e5833d3
filipjt88 <filip88ks@gmail.com>
2025-12-21 21:53:23 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
-import CarDetails from "./components/pages/CarDetails";
+// import CarDetails from "./components/pages/CarDetails";
 
 export default function App() {
   return (
-    <BrowserRouter>
+     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/car/:id" element={<CarDetails/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
