c1003652a6468a4b22668a701a1c383ddc8cdfb3
filipjt88 <filip88ks@gmail.com>
2025-12-21 20:36:59 +0100

    update

--- backend/api/cars.php
+++ backend/api/cars.php
@@ -1,35 +1,48 @@
 <?php
 require_once "../config/db.php";
 
-$sql = "SELECT * FROM cars WHERE 1 = 1";
+
+$sql = "SELECT c.*, GROUP_CONCAT(i.path ORDER BY i.is_main DESC) AS images FROM cars c LEFT JOIN images i ON i.car_id = c.id WHERE 1=1";
+
+
 $params = [];
 
 if(!empty($_GET['brand'])) {
-    $sql .= " AND brand = :brand";
+    $sql .= " AND c.brand = :brand";
     $params['brand'] = $_GET['brand'];
 }
 
 if(!empty($_GET['model'])) {
-    $sql .= " AND model = :model";
+    $sql .= " AND c.model = :model";
     $params['model'] = $_GET['model'];
 }
 
 if(!empty($_GET['fuel'])) {
-    $sql .= " AND fuel = :fuel";
+    $sql .= " AND c.fuel = :fuel";
     $params['fuel'] = $_GET['fuel'];
 }
 
 if(!empty($_GET['priceTo'])) {
-    $sql .= " AND priceTo = :priceTo";
-    $params['priceTo'] = $_GET['priceTo'];
+    $sql .= " AND c.price = :priceTo";
+    $params['priceTo'] = (int)$_GET['priceTo'];
+}
+
+if(!empty($_GET['yearFrom'])) {
+    $sql .= " AND c.year = :yearFrom";
+    $params['yearFrom'] = (int)$_GET['yearFrom'];
 }
 
 if(!empty($_GET['yearTo'])) {
     $sql .= " AND yearTo = :yearTo";
-    $params['yearTo'] = $_GET['yearTo'];
+    $params['yearTo'] = (int)$_GET['yearTo'];
 }
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute($params);
+$cars = $stmt->fetchAll();
 
-echo json_encode($stmt->fetchAll());
+foreach ($cars as $car) {
+    $car['images'] = $car['images'] ? explode(',', $car['images']) : [];
+}
+
+echo json_encode($cars);
