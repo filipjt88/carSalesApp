a84bfa6cc69bfdfc71e59e894b83e88d8c51120c
filipjt88 <filip88ks@gmail.com>
2025-12-24 13:28:48 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import { AuthProvider } from './context/AuthContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
