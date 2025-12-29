2ddc8aaac68e1bf76d7461eaa21a7ec1f7f543eb
filipjt88 <filip88ks@gmail.com>
2025-12-29 21:56:46 +0100

    create function createCar

--- src/components/pages/CreateCar.jsx
+++ src/components/pages/CreateCar.jsx
@@ -3,3 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const API = "http://localhost/carSalesApp/backend/api/car_create.php";
+
+export default function CreateCar() {
+    
+}
