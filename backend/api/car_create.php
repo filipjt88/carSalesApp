6ae22be2fb7dddcaf1ff5d54810fdc43c135ec4b
filipjt88 <filip88ks@gmail.com>
2025-12-29 23:47:02 +0100

    execute

--- backend/api/car_create.php
+++ backend/api/car_create.php
@@ -5,3 +5,20 @@ session_start();
 $user_id = 1;
 
 $stmt = $pdo->prepare("INSERT INTO cars (user_id, brand, model, year, price, km, fuel, gearbox, body, city, description" VALUES(?,?,?,?,?,?,?,?,?,?,?));
+
+$stmt->execute([
+    $user_id,
+    $_POST['brand'],
+    $_POST['model'],
+    $_POST['year'],
+    $_POST['price'],
+    $_POST['km'],
+    $_POST['fuel'],
+    $_POST['gearbox'],
+    $_POST['body'],
+    $_POST['city'],
+    $_POST['description']
+]);
+
+$car_id = $pdo=>lastInsertId();
+
