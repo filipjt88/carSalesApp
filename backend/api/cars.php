7b9dcdfb220846c5ff3bb0e35d63e4672c818042
filipjt88 <filip88ks@gmail.com>
2025-12-29 21:44:19 +0100

    edit cars filter

--- backend/api/cars.php
+++ backend/api/cars.php
@@ -5,47 +5,45 @@ header("Content-Type: application/json");
 require_once "../config/db.php";
 
 
-$sql = "SELECT * FROM cars WHERE 1=1;
+$sql = "SELECT * FROM cars WHERE 1=1";
 
 
 $params = [];
 
-if(!empty($_GET['brand'])) {
-    $sql .= " AND c.brand = :brand";
-    $params['brand'] = $_GET['brand'];
+if (!empty($_GET['brand'])) {
+    $sql .= " AND brand = :brand";
+    $params[':brand'] = $_GET['brand'];
 }
 
-if(!empty($_GET['model'])) {
-    $sql .= " AND c.model = :model";
-    $params['model'] = $_GET['model'];
+if (!empty($_GET['model'])) {
+    $sql .= " AND model = :model";
+    $params[':model'] = $_GET['model'];
 }
 
-if(!empty($_GET['fuel'])) {
-    $sql .= " AND c.fuel = :fuel";
-    $params['fuel'] = $_GET['fuel'];
+if (!empty($_GET['fuel'])) {
+    $sql .= " AND fuel = :fuel";
+    $params[':fuel'] = $_GET['fuel'];
 }
 
-if(!empty($_GET['priceTo'])) {
-    $sql .= " AND c.price = :priceTo";
-    $params['priceTo'] = (int)$_GET['priceTo'];
+if (!empty($_GET['priceTo'])) {
+    $sql .= " AND price = :priceTo";
+    $params[':priceTo'] = $_GET['priceTo'];
 }
 
 if(!empty($_GET['yearFrom'])) {
-    $sql .= " AND c.year = :yearFrom";
-    $params['yearFrom'] = (int)$_GET['yearFrom'];
+    $sql .= " AND year >= :yearFrom";
+    $params[':yearFrom'] = $_GET['yearFrom'];
 }
 
-if(!empty($_GET['yearTo'])) {
-    $sql .= " AND yearTo = :yearTo";
-    $params['yearTo'] = (int)$_GET['yearTo'];
+if (!empty($_GET['yearTo'])) {
+    $sql .= " AND yearTo <= :yearTo";
+    $params[':yearTo'] = (int)$_GET['yearTo'];
 }
+
+$sql .= " ORDER BY created_at DESC";
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute($params);
-$cars = $stmt->fetchAll();
 
-foreach ($cars as $car) {
-    $car['images'] = $car['images'] ? explode(',', $car['images']) : [];
-}
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 
-echo json_encode($cars);
