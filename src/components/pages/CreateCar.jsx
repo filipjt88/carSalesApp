2a4af76bfdaa94e48b011dd029a6f457fef5ebec
filipjt88 <filip88ks@gmail.com>
2025-12-29 21:55:49 +0100

    add api link car crate

--- src/components/pages/CreateCar.jsx
+++ src/components/pages/CreateCar.jsx
@@ -1,3 +1,5 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
+const API = "http://localhost/carSalesApp/backend/api/car_create.php";
