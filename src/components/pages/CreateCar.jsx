30ae6c04b6334106a1c8de81b86f42b2067c397b
filipjt88 <filip88ks@gmail.com>
2025-12-29 21:59:23 +0100

    add form and setform

--- src/components/pages/CreateCar.jsx
+++ src/components/pages/CreateCar.jsx
@@ -5,5 +5,19 @@ import { useNavigate } from "react-router-dom";
 const API = "http://localhost/carSalesApp/backend/api/car_create.php";
 
 export default function CreateCar() {
-    
+    const navigate = useNavigate();
+
+    const [form, setForm] = useState({
+        brand: "",
+        model: "",
+        year: "",
+        price: "",
+        km: "",
+        fuel: "",
+        hp:"",
+        gearbox: "",
+        body: "",
+        description: "",
+        city:""
+    });
 }
