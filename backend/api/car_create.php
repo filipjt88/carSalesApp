2f027679a0706a13c3d37e8c73621a52d001f8c8
filipjt88 <filip88ks@gmail.com>
2025-12-29 23:44:55 +0100

    insert into cars

--- backend/api/car_create.php
+++ backend/api/car_create.php
@@ -4,3 +4,4 @@ session_start();
 
 $user_id = 1;
 
+$stmt = $pdo->prepare("INSERT INTO cars (user_id, brand, model, year, price, km, fuel, gearbox, body, city, description" VALUES(?,?,?,?,?,?,?,?,?,?,?));
