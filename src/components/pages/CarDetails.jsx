54799d8b2adb93719641fd2ee3f0dc1ff3850995
filipjt88 <filip88ks@gmail.com>
2025-12-30 23:07:16 +0100

    set car params

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -1,9 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const API = "http://localhost/carSalaesApp/backend/api/car.php";
 
 export default function CarDetails() {
-    
+    const { id } = useParams();
+    const [car, setCar] = useState(null);
+
+    useEffect(() => {
+        axios.get(API, { params: { id } }).then(res => {
+            setCar(res.data);
+        });
+    }, [id]);
 }
