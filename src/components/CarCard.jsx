809a3b1af97dd1fcacd3401cd713c01f9de026f8
filipjt88 <filip88ks@gmail.com>
2025-12-30 23:25:16 +0100

    updated car card

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -2,23 +2,31 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 export default function CarCard({ car }) {
-    return (
-        <div className="col-md-4 mb-4">
-            <div className="card shadow-sm">
-                <img src={car.images?.[0] || "https://via.placeholder.com/400x250"} className="car-img-top" alt={car.model} />
-                <div className="card-body">
-                    <h5 className="card-title">
-                        {car.brand} {car.model}
-                    </h5>
-                    <p className="card-text">
-                        Godiste: {car.year} <br/>
-                        Kilometraza: {car.km.toLocaleString()} km <br/>
-                        Cena: {car.price} evra
-                    </p>
-                    <Link to={`/car/${car.id}`} className="btn btn-primary w-100">
-                    Detaljnije</Link>
-                </div>
-            </div>
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card h-100 shadow-sm">
+
+        <img
+          src={`http://localhost/carSalesApp/backend/uploads/${car.image}`}
+          className="card-img-top"
+          style={{ height: 200, objectFit: "cover" }}
+        />
+
+        <div className="card-body">
+          <h5>{car.brand} {car.model}</h5>
+          <p className="mb-1"><b>{car.price} €</b></p>
+          <small className="text-muted">
+            {car.year} • {car.km} km • {car.fuel}
+          </small>
         </div>
-    )
+
+        <div className="card-footer bg-white border-0">
+          <Link to={`/cars/${car.id}`} className="btn btn-outline-primary w-100">
+            Pogledaj oglas
+          </Link>
+        </div>
+
+      </div>
+    </div>
+  );
 }
