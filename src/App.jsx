20cf6f84f00f4abd92b9cb9575797ded234de661
filipjt88 <filip88ks@gmail.com>
2025-12-30 23:20:34 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./components/pages/Register";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import CreateCar from "./components/pages/CreateCar";
+import CarDetails from "./components/pages/CarDetails";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cars/:id" element={<CreateCar/>} />
+        <Route path="/cars/:id" element={<CarDetails/>} />
+        <Route path="/create-car" element={<CreateCar/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
