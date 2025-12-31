ceb98deb11ba1d3921c1b92bd1c1008f24b49ceb
filipjt88 <filip88ks@gmail.com>
2025-12-31 19:42:33 +0100

    Add city

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -47,6 +47,7 @@ return (
                 <li className="list-group-item">Kilometraza: {car.km}</li>
                 <li className="list-group-item">Gorivo: {car.fuel}</li>
                 <li className="list-group-item">Menjac: {car.gearbox}</li>
+                <li className="list-group-item">Grad: {car.city}</li>
             </ul>
         </div>
 
