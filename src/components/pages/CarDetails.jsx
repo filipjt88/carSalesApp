4da6ef0c5d686d0b1e2ed3093e1c0d3d276133f2
filipjt88 <filip88ks@gmail.com>
2025-12-31 19:40:20 +0100

    add list group

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -41,6 +41,10 @@ return (
         <div className="col-md-5">
             <h3>{car.brand} {car.model}</h3>
             <h4 className="text-primary mb-3">{car.price}  €</h4>
+
+            <ul className="list-group mb-3">
+                
+            </ul>
         </div>
 
     </div>
