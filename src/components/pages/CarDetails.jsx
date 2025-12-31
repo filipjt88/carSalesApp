45955dbf8d2bce8e68707435099fd5cb4c6b6016
filipjt88 <filip88ks@gmail.com>
2025-12-31 19:32:15 +0100

    .

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -22,20 +22,7 @@ if (!car) return <p className="text-center mt-5">Ucitavanje ...</p>
 
 return (
     <div className="container mt-4">
-        <h2>{car.brand} {car.model}</h2>
-
-        <img src={`http://localhost/carSalesApp/backend/uploads/${car.images[0]}`} className="img-fluid mb-3" />
         
-        <h4>{car.price} €</h4>
-
-        <ul className="list-group">
-            <li className="list-group-item">Godiste: {car.year}</li>
-            <li className="list-group-item">Kilometraza: {car.km}</li>
-            <li className="list-group-item">Gorivo: {car.fuel}</li>
-            <li className="list-group-item">Menjac: {car.gearbox}</li>
-            <li className="list-group-item">Grad: {car.city}</li>
-        </ul>
-        <p className="mt-3">{car.decription}</p>
     </div>
 )
 }
