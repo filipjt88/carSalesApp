84d499f60f8cdbd020f4cba43c4813ada433fb6a
filipjt88 <filip88ks@gmail.com>
2025-12-31 19:46:42 +0100

    comm

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -10,7 +10,7 @@ export default function CarDetails() {
     const [car, setCar] = useState(null);
     const [mainImage, setMainImage] = useState("");
 
-    
+
     useEffect(() => {
         axios.get(API, { params: { id } }).then(res => {
             setCar(res.data);
@@ -24,6 +24,7 @@ if (!car || !mainImage) return <p className="text-center mt-5">Ucitavanje ...</p
 
 return (
     <div className="container mt-4">
+
         {/* Galerija */}
         <div className="col-md-7">
             <img src={IMG + mainImage} className="img-fluid rounded mb-3" style={{maxHeight: 420, objectFit: "cover", width: "100%"}} />
@@ -40,6 +41,7 @@ return (
         </div>
 
         {/* Podaci */}
+        
         <div className="col-md-5">
             <h3>{car.brand} {car.model}</h3>
             <h4 className="text-primary mb-3">{car.price}  €</h4>
