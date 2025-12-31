9d6c5d501c6be6f6640ec8c885a7b07a9404814d
filipjt88 <filip88ks@gmail.com>
2025-12-31 19:30:33 +0100

    save

--- src/components/pages/CarDetails.jsx
+++ src/components/pages/CarDetails.jsx
@@ -13,6 +13,7 @@ export default function CarDetails() {
     useEffect(() => {
         axios.get(API, { params: { id } }).then(res => {
             setCar(res.data);
+            setMainImage(res.data.images?.[0]);
         });
     }, [id]);
 
