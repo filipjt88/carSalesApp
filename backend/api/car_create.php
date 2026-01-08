e05ab2098e4fa91aeb0c1d89c8566016dd546154
filipjt88 <filip88ks@gmail.com>
2026-01-08 21:30:30 +0100

    add content type app

--- backend/api/car_create.php
+++ backend/api/car_create.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST");
+header("Content-Type: aplication/json");
 require "../config/db.php";
 
 
