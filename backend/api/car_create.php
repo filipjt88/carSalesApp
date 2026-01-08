868ad47dedf65acf5d6afb55d18424c5db2dde56
filipjt88 <filip88ks@gmail.com>
2026-01-08 21:02:37 +0100

    add access allow orogin

--- backend/api/car_create.php
+++ backend/api/car_create.php
@@ -1,6 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 require "../config/db.php";
-session_start();
 
 
 $brand       = $_POST['brand'] ?? '';
@@ -13,46 +13,3 @@ $gearbox     = $_POST['gearbox'] ?? '';
 $body        = $_POST['body'] ?? '';
 $city        = $_POST['city'] ?? '';
 $description = $_POST['description'] ?? '';
-
-$user_id = 1;
-
-$sql = "
-INSERT INTO cars 
-(
-    brand, model, year, price, km, fuel, gearbox, body, city, description
-)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-";
-
-$stmt = $pdo->prepare($sql);
-
-$stmt->execute([
-    $user_id,
-    $_POST['brand'],
-    $_POST['model'],
-    $_POST['year'],
-    $_POST['price'],
-    $_POST['km'],
-    $_POST['fuel'],
-    $_POST['gearbox'],
-    $_POST['body'],
-    $_POST['city'],
-    $_POST['description']
-]);
-
-$car_id = $pdo->lastInsertId();
-
-
-$uploadDir = "../uploads";
-$isMain = 1;
-
-foreach ($_FILES['images']['tmp_name'] as $i => $tmp) {
-    $name = time(). "_" . $_FILES['images']['name'][$i];
-    move_uploaded_file($tmp, $uploadDir . $name);
-
-    $stmt = $pdo->prepare("INSERT INTO images (car_id, path, is_main) VALUES (?,?,?)");
-    $stmt->execute([$car_id, $name, $isMain]);
-    $isMain = 0;
-}
-
-echo json_encode(["success" => true]);
