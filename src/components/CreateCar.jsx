aeb59ca4233dd6fc515ecee0745e5d9863201729
filipjt88 <filip88ks@gmail.com>
2026-01-09 21:10:58 +0100

    add

--- src/components/CreateCar.jsx
+++ src/components/CreateCar.jsx
@@ -4,5 +4,16 @@ import { useState } from "react";
 const API_URL = "http://localhost/carSalesApp/backend/api/car_create.php";
 
 export default function CreateCar() {
-        
+    const [form, setForm] = useState({
+        brand: "",
+        model: "",
+        year: "",
+        price: "",
+        km: "",
+        fuel: "",
+        gearbox: "",
+        body: "",
+        city: "",
+        description: ""
+    });
 }
