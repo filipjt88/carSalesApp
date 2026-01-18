e74ac4c652c88cd1b56915bdb1cfc0ee65b7acb6
filipjt88 <filip88ks@gmail.com>
2026-01-18 20:53:34 +0100

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ export default function Header() {
                 <h4>Prodaja polovnih automobila <i className="fa-solid fa-car"></i></h4>
                 </Link>
                 <form className="d-flex ms-auto" style={{maxWidth:"400px"}}>
-                    <input className="form-control me-2" type="search" placeholder="Pretraga..." />
-                    <button className="btn btn-outline-light" type="submit">Pretraga</button>
+                    <input className="form-control me-2" type="search" placeholder="Pretrazi..." />
+                    <button className="btn btn-outline-light" type="submit">Pretrazi</button>
                 </form>
             </div>
         </nav>
