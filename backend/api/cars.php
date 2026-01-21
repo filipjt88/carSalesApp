545eebf4d89c990ce03e4e3591ffae3204cafb25
filipjt88 <filip88ks@gmail.com>
2026-01-21 21:58:53 +0100

    .

--- backend/api/cars.php
+++ backend/api/cars.php
@@ -10,17 +10,17 @@ $params = [];
 
 if (!empty($_GET['brand'])) {
     $sql .= " AND brand = :brand";
-    $params[':brand'] = $_GET['brand'];
+    $params[':brand']   = $_GET['brand'];
 }
 
 if (!empty($_GET['model'])) {
     $sql .= " AND model = :model";
-    $params[':model'] = $_GET['model'];
+    $params[':model']   = $_GET['model'];
 }
 
 if (!empty($_GET['fuel'])) {
     $sql .= " AND fuel = :fuel";
-    $params[':fuel'] = $_GET['fuel'];
+    $params[':fuel']   = $_GET['fuel'];
 }
 
 if (!empty($_GET['priceTo'])) {
