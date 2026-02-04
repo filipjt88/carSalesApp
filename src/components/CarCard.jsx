515a2c75b2bb5dc033b0c0dacd79f3feb9bc39ad
filipjt88 <filip88ks@gmail.com>
2026-02-04 20:39:57 +0100

    save

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "../App.css";
 
-// Card
+// Car card oglas
 export default function CarCard({ car }) {
   return (
     <div className="col-md-4 mb-4">
