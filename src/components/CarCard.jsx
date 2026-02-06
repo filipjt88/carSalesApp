e2cbd40f3345eaec71ca9214a13a0f5171d028bb
filipjt88 <filip88ks@gmail.com>
2026-02-06 21:03:21 +0100

    save

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -7,10 +7,10 @@ export default function CarCard({ car }) {
     <div className="col-md-4 mb-4">
       <div className="card h-100 shadow-sm">
         <img src={`http://localhost/carSalesApp/backend/uploads/${car.image}`} className="card-img-top"
-          style={{ height: 200, objectFit: "cover" }}/>
+          style={{ height: 250, objectFit: "cover" }}/>
         <div className="card-body">
           <h5>{car.brand} {car.model}</h5>
-          <p className="mb-1"><b>{car.price} €</b></p>
+          <p className="mb-1"> <b>{car.price} €</b> </p>
           <small className="text-muted">
             {car.year} * {car.km} km * {car.fuel}
           </small>
