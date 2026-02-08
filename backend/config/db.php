0727a968fe391b3d1565820874dba5580ec72750
filipjt88 <filip88ks@gmail.com>
2026-02-08 22:23:51 +0100

    .

--- backend/config/db.php
+++ backend/config/db.php
@@ -16,6 +16,6 @@ try {
     );
 } catch (PDOException $e) {
     http_response_code(500);
-    echo json_encode(["error" => "DB connection failed!"]);
+    echo json_encode(["error" => "DB connection failed!!"]);
     exit;
 }
